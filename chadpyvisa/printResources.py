5dfd6ec0e97f3e4b8f9e30c9a96716a240e682b4
tyingz <tomynuezprogram@gmail.com>
2026-05-13 16:27:57 -0300

    asd

--- chadpyvisa/printResources.py
+++
@@ -1,7 +0,0 @@
-def printResources():
-    rm = pyvisa.ResourceManager()
-    recursos = rm.list_resources()
-    for i in recursos:
-        recursoI = rm.open_resource(i)
-        print(f"el recursos {i} es: {recursoI.query('*IDN?').strip()}")
-
